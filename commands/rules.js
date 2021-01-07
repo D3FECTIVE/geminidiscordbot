025daf514bedd352b85384cdcfb33d7763847ab7
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-01-06 21:51:31 -0800

    Rules V3

--- commands/rules.js
+++ commands/rules.js
@@ -11,7 +11,11 @@ module.exports = {
         .setTitle('**__Server Rules!__**')
         .setDescription('**1. Use English only.**\n\n\n**2. No bullying, harrassment, or hate speech.**\n\n\n**3. Do not spam.**\n\n\n**4. No self-promotions or advertisements.**\n\n\n**5. No political or religious topics.**\n\n\n**6. Do not @mention or DM staff without a good reason.**\n\n\n**7. NSFW content is prohibited.**\n\n\n**8. Be Respectful and use common sense.**\n\n\n**9. Staff have final say.**\n\n\n**10. Discord ToS applies to this server. ** https://discordapp.com/tos');
 
+        const HelpEmbed = new Discord.MessageEmbed()
+        .setColor('#0a0a0a')
+        .setDescription('**Invite link: https://discord.gg/w6v2fHg**');
+
         message.channel.send(RulesEmbed)
-        
+        message.channel.send(HelpEmbed);
     }
 }
