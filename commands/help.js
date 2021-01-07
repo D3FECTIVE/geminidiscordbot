db6929a48808a712d2ddc2c94c2784ba229aea16
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-01-07 15:42:56 -0800

    Help V3

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         const HelpEmbed = new Discord.MessageEmbed()
         .setColor('#03122c')
         .setTitle('Commands List')
-        .setDescription('Many of the commands listed below are limited to staff members, or special server roles only granted to few.\n\n\n***__Public Commands__***\n\nDiscord Server Invite```g.invite```\n\n\n***__Staff Commands__***\n\nAnnouncements```g.say EXAMPLETEXT```\n**Rules**```g.rules```\nRealm Rules```g.mcrules```\nClan Rules```g.clanrules```\n');
+        .setDescription('Many of the commands listed below are limited to staff members, or special server roles only granted to few.\n\n\n***__Public Commands__***\n\nDiscord Server Invite```g.invite```\n\n\n***__Staff Commands__***\n\nAnnouncements```g.say EXAMPLETEXT```\nRules```g.rules```\nRealm Rules```g.mcrules```\nClan Rules```g.clanrules```\n');
 
         message.channel.send(HelpEmbed);
     }
