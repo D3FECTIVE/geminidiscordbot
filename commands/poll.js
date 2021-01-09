1f1c59b5618cdc055e8b0b7e2feb79201cf68940
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-01-09 01:11:54 -0800

    Poll test v3

--- commands/poll.js
+++ commands/poll.js
@@ -6,17 +6,18 @@ module.exports = {
     async execute(message, args){
         if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("**You're not authorized to use this command!**")
 
-        let argresult = args.slice(1).join("** **")
+        let argresult = args.slice(1).join(" ")
         let mChannel = message.mentions.channels.first()
 
         message.delete()
         let PollEmbed = new Discord.MessageEmbed()
         .setColor('#00d0ff')
+        .setTitle(argresult)
         .setDescription(argresult)
+        mChannel.send('@here');
         let msgEmbed = await mChannel.send(PollEmbed);
         await msgEmbed.react('707644906260398190')
         await msgEmbed.react('707644956151513179')
-        mChannel.send('@here');
 
     }
 }
