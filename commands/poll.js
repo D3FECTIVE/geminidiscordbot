5d69b4bfd803558aa435adc1f8bbf59ed832df75
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-01-09 01:47:24 -0800

    Poll test v7

--- commands/poll.js
+++ commands/poll.js
@@ -16,7 +16,7 @@ module.exports = {
             argresult = args.slice(1).join(" ")
 
             let PollEmbed = new Discord.MessageEmbed()
-            .setColor('#0a0a0a')
+            .setColor('#00d0ff')
             .setDescription(argresult)
 
             let msgEmbed = await mChannel.send(PollEmbed);
@@ -30,7 +30,7 @@ module.exports = {
             argresult = args.join(" ")
 
             let PollEmbed = new Discord.MessageEmbed()
-            .setColor('#0082ff')
+            .setColor('#efff00')
             .setDescription(argresult)
 
             let msgEmbed = await message.channel.send(PollEmbed);
