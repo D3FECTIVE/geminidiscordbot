56a2337724be3b0dd4d7a5340e74547a330d3580
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-01-09 02:03:59 -0800

    Poll testing Green

--- commands/poll.js
+++ commands/poll.js
@@ -30,7 +30,7 @@ module.exports = {
             argresult = args.join(" ")
 
             let PollEmbed = new Discord.MessageEmbed()
-            .setColor('#75e7bf')
+            .setColor('#2cff00')
             .setDescription(argresult)
 
             let msgEmbed = await message.channel.send(PollEmbed);
