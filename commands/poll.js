852eb2ff100adbf73aa4f8921731facfdfc998de
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-01-09 01:42:11 -0800

    Poll test v6

--- commands/poll.js
+++ commands/poll.js
@@ -6,20 +6,36 @@ module.exports = {
     async execute(message, args){
         if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("**You're not authorized to use this command!**")
 
-        let argresult = args.slice(1).join(" ")
+        let argresult;
         let mChannel = message.mentions.channels.first()
 
         message.delete()
+        if(mChannel) {
+            mChannel.send('@everyone');
 
-        mChannel.send('@everyone');
+            argresult = args.slice(1).join(" ")
 
-        let PollEmbed = new Discord.MessageEmbed()
-        .setColor('#0082ff')
-        .setDescription(argresult)
-        let msgEmbed = await mChannel.send(PollEmbed);
+            let PollEmbed = new Discord.MessageEmbed()
+            .setColor('#0a0a0a')
+            .setDescription(argresult)
 
-        await msgEmbed.react('707644906260398190')
-        await msgEmbed.react('707644956151513179')
+            let msgEmbed = await mChannel.send(PollEmbed);
+            await msgEmbed.react('707644906260398190')
+            await msgEmbed.react('707644956151513179')
 
+        } else {
+
+            message.channel.send('@everyone');
+
+            argresult = args.join(" ")
+
+            let PollEmbed = new Discord.MessageEmbed()
+            .setColor('#0082ff')
+            .setDescription(argresult)
+
+            let msgEmbed = await message.channel.send(PollEmbed);
+            await msgEmbed.react('707644906260398190')
+            await msgEmbed.react('707644956151513179')
+        }
     }
 }
