0fdaab769edf5010ac5265b153543458263fe64a
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-02-07 18:16:51 -0800

    1.2.9

--- commands/rules.js
+++ commands/rules.js
@@ -9,55 +9,47 @@ module.exports = {
         const RulesEmbed1 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 1__**')
-        .setDescription('**1. Use English only.**\n\nEnglish is the primary language of this server, no using languages other than English.');
+        .setDescription('**Use English only.**\n\nEnglish is the primary language of this server, no using languages other than English.');
 
         const RulesEmbed2 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 2__**')
-        .setDescription('**No bullying, harrassment, or hate speech.**')
-        .setFooter('Discrimination, trolling, and harassment is not tolerated. Personal attacks on a user, will result in a mute without warning, and a ban if continued.');
+        .setDescription('**No bullying, harrassment, or hate speech.**\n\nDiscrimination, trolling, and harassment is not tolerated. Personal attacks on a user, will result in a mute without warning, and a ban if continued.');
 
         const RulesEmbed3 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 3__**')
-        .setDescription('**Keep conversations and content in their respective channels. Do not spam.**')
-        .setFooter('Every channel in this server has a description, please read it before sending a message. No spamming is allowed. Staff Mailbox in this server can only be used for moderation issues, or reporting a game bug.');
+        .setDescription('**Keep conversations and content in their respective channels. Do not spam.**\n\nEvery channel in this server has a description, please read it before sending a message. No spamming is allowed. Staff Mailbox in this server can only be used for moderation issues, or reporting a game bug.');
 
         const RulesEmbed4 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 4__**')
-        .setDescription('**No self-promotions or advertisements.**')
-        .setFooter('This server is not a sales platform or a hub for other servers. Advertising, whether through DMs, chat, or images is prohibited. In-direct advertising which does not include an invite link is also prohibited.');
+        .setDescription('**No self-promotions or advertisements.**\n\nThis server is not a sales platform or a hub for other servers. Advertising, whether through DMs, chat, or images is prohibited. In-direct advertising which does not include an invite link is also prohibited.');
 
         const RulesEmbed5 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 5__**')
-        .setDescription('**No political or religious topics.**')
-        .setFooter('No controversial content allowed.');
+        .setDescription('**No political or religious topics.**\n\nNo controversial content allowed.');
 
         const RulesEmbed6 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 6__**')
-        .setDescription('**Do not @mention or DM staff.**')
-        .setFooter('This includes begging for a role or in game currencies. If you wanna report a user DM the Staff Mailbox to open a support ticket.');
+        .setDescription('**Do not @mention or DM staff.**\n\nThis includes begging for a role or in game currencies. If you wanna report a user DM the Staff Mailbox to open a support ticket.');
 
         const RulesEmbed7 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 7__**')
-        .setDescription('**NSFW content is prohibited.**')
-        .setFooter('Refrain from all NSFW discussions. Images or links to NSFW sites are not allowed in this server. All content sent on this server must be PG-13. This also includes your username and avatar. Violating this rule will result in an immediate ban from this server.');
+        .setDescription('**NSFW content is prohibited.**\n\nRefrain from all NSFW discussions. Images or links to NSFW sites are not allowed in this server. All content sent on this server must be PG-13. This also includes your username and avatar. Violating this rule will result in an immediate ban from this server.');
 
         const RulesEmbed8 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 8__**')
-        .setDescription('**Be Respectful and use common sense.**')
-        .setFooter('Respect all members in the server. Refrain from flaming, trolling, or being disrespectful. Racial slurs and discrimination are strictly prohibited. Respecting a user’s privacy also falls under this. Information regarding a user’s real identity should not be posted without their permission.');
+        .setDescription('**Be Respectful and use common sense.**\n\nRespect all members in the server. Refrain from flaming, trolling, or being disrespectful. Racial slurs and discrimination are strictly prohibited. Respecting a user’s privacy also falls under this. Information regarding a user’s real identity should not be posted without their permission.');
 
         const RulesEmbed9 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rule 9__**')
-        .setDescription('**Staff have final say.**')
-        .setFooter('Staff have the final say in all verdicts. Failure to listen to staff will result in punishment. Do not talk back to staff.');
+        .setDescription('**Staff have final say.**\n\nStaff have the final say in all verdicts. Failure to listen to staff will result in punishment. Do not talk back to staff.');
 
         const RulesEmbed10 = new Discord.MessageEmbed()
         .setColor('#2cff00')
