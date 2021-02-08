8213d0c375224fdbc15238d3d729c39bf03101dc
DevAlexPlayzxx <73058004+DevAlexPlayzxx@users.noreply.github.com>
2021-02-07 16:13:39 -0800

    1.1.1

--- commands/rules.js
+++ commands/rules.js
@@ -6,13 +6,13 @@ module.exports = {
     execute(message, args){
         if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("**You're not authorized to use this command!**")
         
-        const RulesEmbed = new Discord.MessageEmbed()
+        const RulesEmbed1 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rules 1__**')
         .setDescription('**1. Use English only.**\n\n\n**2. No bullying, harrassment, or hate speech.**\n\n\n**3. Do not spam.**\n\n\n**4. No self-promotions or advertisements.**\n\n\n**5. No political or religious topics.**\n\n\n**6. Do not @mention or DM staff without a good reason.**\n\n\n**7. NSFW content is prohibited.**\n\n\n**8. Be Respectful and use common sense.**\n\n\n**9. Staff have final say.**\n\n\n**10. Discord ToS applies to this server. ** https://discordapp.com/tos')
         .setFooter('Testing');
 
-        const RulesEmbed = new Discord.MessageEmbed()
+        const RulesEmbed2 = new Discord.MessageEmbed()
         .setColor('#2cff00')
         .setTitle('**__Rules 2__**')
         .setDescription('**No bullying, harrassment, or hate speech');
@@ -22,7 +22,8 @@ module.exports = {
         .setDescription('**Invite link: https://discord.gg/w6v2fHg**');
 
         message.delete()
-        message.channel.send(RulesEmbed)
+        message.channel.send(RulesEmbed1)
+        message.channel.send(RulesEmbed2)
         message.channel.send(HelpEmbed);
     }
 }
