03950f9589cc00e9c1a38093072a910db187007c
DevAlexPlayzxx <akfilms00@gmail.com>
2021-11-08 18:59:14 -0800

    Handler v13

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
-// New Code
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const fs = require('fs');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+
+const prefix = 'g.';
+
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands");
+
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
+    client.login(process.env.token);
+})();
+
+/*
 client.once('ready', () => {
     console.log('Gemini is online!');
 });
 
-/*
+
 Old Code
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
@@ -101,5 +119,3 @@ client.on('message', message =>{
     }
 })
 */
-
-client.login(process.env.token);
