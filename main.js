3520ad248827586fd5dd768be3cdaecd5c23750b
DevAlexPlayzxx <akfilms00@gmail.com>
2021-11-08 16:26:23 -0800

    2.0.300

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
-const Discord = require('discord.js');
+// New Code
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+require('dotenv').config();
 
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+client.on('ready', () => {
+    console.log('Gemini is online!');
+});
+
+(async () => {
+    client.login(process.env.token);
+})();
+
+/*
+Old Code
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [{
+    name: 'ping',
+    description: 'Replies with Pong!'
+}];
+
+const rest = new REST({ version: '9'}).setToken('token');
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+ //const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const prefix = 'g.';
 
@@ -64,5 +105,6 @@ client.on('message', message =>{
         client.commands.get('afk').execute(message, args);
     }
 })
+*/
 
-client.login(process.env.token);
+//client.login(process.env.token);
